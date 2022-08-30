fd21050210dad4934cdebb54a442ed50b9f8c56d
Axel Somerseth Córdova López <axelsomerseth@gmail.com>
2022-08-29 22:31:47 -0600

    inserted data into categories table

---
+++ db/2022-08-30T04:13:00.000Z_add_data_to_category_table.sql
@@ -0,0 +1,23 @@
+INSERT INTO
+	categories (
+		name,
+		description,
+		created_at,
+		updated_at,
+		deleted_at
+	)
+VALUES
+	(
+		'Bebidas Alcoholicas',
+		'Bebidas Alcoholicas como cerveza, licores, rones, vinos, aguardiente, etc.',
+		CURRENT_TIMESTAMP,
+		CURRENT_TIMESTAMP,
+		NULL
+	),
+	(
+		'Cigarros',
+		'Cigarrillos',
+		CURRENT_TIMESTAMP,
+		CURRENT_TIMESTAMP,
+		NULL
+	);
