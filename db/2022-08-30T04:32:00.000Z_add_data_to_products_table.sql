982fa4e2ca8d05ee7a5a1a1834099c8adf60a622
Axel Somerseth Córdova López <axelsomerseth@gmail.com>
2022-08-29 22:40:51 -0600

    inserted data into products table

---
+++ db/2022-08-30T04:32:00.000Z_add_data_to_products_table.sql
@@ -0,0 +1,52 @@
+INSERT INTO
+	products (
+		name,
+		description,
+		categoty_id,
+		price,
+		stock,
+		created_at,
+		updated_at,
+		deleted_at
+	)
+VALUES
+	(
+		'Cerveza Salva Vida Caguama',
+		'Cerveza Salva Vida Botella Caguama L0.75',
+		3,
+		28,
+		10,
+		CURRENT_TIMESTAMP,
+		CURRENT_TIMESTAMP,
+		NULL
+	),
+	(
+		'Cerveza Salva Vida Lata',
+		'Cerveza Salva Vida Lata 12oz',
+		3,
+		23,
+		10,
+		CURRENT_TIMESTAMP,
+		CURRENT_TIMESTAMP,
+		NULL
+	),
+	(
+		'Cerveza Salva Vida Vidrio normal',
+		'Cerveza Salva Vida Vidrio normal 12oz',
+		3,
+		18,
+		10,
+		CURRENT_TIMESTAMP,
+		CURRENT_TIMESTAMP,
+		NULL
+	),
+	(
+		'Cerveza Victoria Frost lata',
+		'Cerveza Victoria Frost lata 12oz',
+		3,
+		25,
+		10,
+		CURRENT_TIMESTAMP,
+		CURRENT_TIMESTAMP,
+		NULL
+	);
