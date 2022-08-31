179221318b41dd4323f4b5f84040f0e108cff3e6
Axel Somerseth Córdova López <axelsomerseth@gmail.com>
2022-08-30 21:41:57 -0600

    configuring knex with the latest api

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,20 @@
 const path = require("path");
-require("dotenv").config();
+const dotenv = require("dotenv");
+dotenv.config();
 
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
 module.exports = {
   development: {
-    client: "pg",
+    client: "postgresql",
     connection: process.env.PG_CONNECTION_STRING,
+    pool: {
+      min: 2,
+      max: 10,
+    },
     migrations: {
+      tableName: "knex_migrations",
       directory: path.resolve(__dirname + "/db/migrations"),
     },
     seeds: {
@@ -13,9 +22,14 @@ module.exports = {
     },
   },
   production: {
-    client: "pg",
+    client: "postgresql",
     connection: process.env.PG_CONNECTION_STRING,
+    pool: {
+      min: 2,
+      max: 10,
+    },
     migrations: {
+      tableName: "knex_migrations",
       directory: path.resolve(__dirname + "/db/migrations"),
     },
     seeds: {
