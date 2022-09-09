5ccfba1a12305ba18d63acdc7f46b564279f257e
Axel Somerseth Córdova López <axelsomerseth@gmail.com>
2022-09-08 23:20:44 -0600

    seed updated

--- db/seeds/development/01_categories.js
+++ db/seeds/development/01_categories.js
@@ -3,59 +3,51 @@
  * @returns { Promise<void> }
  */
 exports.seed = async function (knex) {
-  // Deletes ALL existing entries
+  // Deletes ALL existing entries.
   await knex("categories").del();
+  // Add new entries.
   await knex("categories").insert([
     {
-      id: 1,
       name: "Bebidas Alcoholicas",
       description: "Cervezas Licores y Vinos",
       created_at: new Date(),
     },
     {
-      id: 2,
       name: "Bebidas y Jugos",
       description: "Aguas, Bebidas Refrescantes, Jugos, Refrescos",
       created_at: new Date(),
     },
     {
-      id: 3,
       name: "Abarrotes",
       description: "",
       created_at: new Date(),
     },
     {
-      id: 4,
       name: "Embutidos",
       description: "",
       created_at: new Date(),
     },
     {
-      id: 5,
       name: "Lácteos",
       description: "Lácteos no Lácteos Derivados y Huevos",
       created_at: new Date(),
     },
     {
-      id: 6,
       name: "Frutas y Verduras",
       description: "",
       created_at: new Date(),
     },
     {
-      id: 7,
       name: "Carnes y Aves",
       description: "",
       created_at: new Date(),
     },
     {
-      id: 8,
       name: "Pescados y Mariscos",
       description: "",
       created_at: new Date(),
     },
     {
-      id: 9,
       name: "Panadería y Tortillas",
       description: "",
       created_at: new Date(),
