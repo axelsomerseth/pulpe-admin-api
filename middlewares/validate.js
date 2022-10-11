185fad5d5694ad15bea77d0a5120cbcd76c28bfc
Axel Somerseth Córdova López <axelsomerseth@gmail.com>
2022-10-10 21:25:06 -0600

    description prop in product schema is required

--- middlewares/validate.js
+++ middlewares/validate.js
@@ -15,14 +15,14 @@ const categorySchema = {
 const productSchema = {
   create: Joi.object({
     name: Joi.string().max(100).required(),
-    description: Joi.string().max(255),
+    description: Joi.string().max(255).required(),
     category_id: Joi.number().required(),
     price: Joi.number().required(),
     stock: Joi.number().required(),
   }),
   update: Joi.object({
     name: Joi.string().max(100).required(),
-    description: Joi.string().max(255),
+    description: Joi.string().max(255).required(),
     category_id: Joi.number().required(),
     price: Joi.number().required(),
     stock: Joi.number().required(),
